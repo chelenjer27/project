d27c31e9c39d4ade33dc0981884f48c4d1a4f370
chelenjer27 <aastolbov@outlook.com>
2022-11-23 08:49:45 +0100

    config.sh add mont fstab

--- work_2/scripts/config.sh
+++ work_2/scripts/config.sh
@@ -14,3 +14,14 @@
             for i in $(seq 1 5);do sudo mkfs.ext4 /dev/md10p$i;done &&
             mkdir -p /raid/part{1,2,3,4,5} &&
             for i in $(seq 1 5);do mount /dev/md10p$i /raid/part$i;done
+            #######mount###############
+            if ! grep -q 'raid10' /etc/fstab ; 
+            then
+            echo '# raid10' >> /etc/fstab
+            echo '/dev/md10p1    /raid/part1    ext4    defaults    0    2' >> /etc/fstab
+            echo '/dev/md10p2    /raid/part2    ext4    defaults    0    2' >> /etc/fstab
+            echo '/dev/md10p3    /raid/part3    ext4    defaults    0    2' >> /etc/fstab
+            echo '/dev/md10p4    /raid/part4    ext4    defaults    0    2' >> /etc/fstab
+            echo '/dev/md10p5    /raid/part5    ext4    defaults    0    2' >> /etc/fstab
+            fi
+
